1fefea06d0ad030e67bde6968f53e8c6b65d9f7b
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+function lazyLoadImages() {
+  const images = document.querySelectorAll('img[data-src]');
+
+  // Configure the Intersection Observer
+  const observerOptions = {
+    root: null, // Use the viewport as the root
+    rootMargin: '0px',
+    threshold: 0.1, // Trigger the callback when 10% of the image is visible
+  };
+
+  const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const image = entry.target;
+        const src = image.getAttribute('data-src');
+
+        // Load the image asynchronously
+        image.setAttribute('src', src);
+        image.removeAttribute('data-src');
+
+        // Stop observing the image
+        observer.unobserve(image);
+      }
+    });
+  }, observerOptions);
+
+  // Start observing the images
+  images.forEach((image) => {
+    imageObserver.observe(image);
+  });
+}
+
+// Call the lazyLoadImages function when the page is loaded
+window.addEventListener('load', lazyLoadImages);
